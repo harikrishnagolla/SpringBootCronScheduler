f75efdc604d507678ad4fef3563e1fceb672b959
Hari Krishna Golla <krishnahari.golla@gmail.com>
2019-11-19 02:36:19 +0530

    Initial commit

---
+++ src/test/java/com/scehduler/SchedulerSample/SchedulerSampleApplicationTests.java
@@ -0,0 +1,15 @@
+/*
+package com.scehduler.SchedulerSample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulerSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+*/
